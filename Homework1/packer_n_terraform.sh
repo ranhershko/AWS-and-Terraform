0143da32069043b2d0895851355534600e579161
Ran Hershko <ran.hershko@gmail.com>
2019-11-12 23:29:10 +0200

    delete nested if

--- Homework1/packer_n_terraform.sh
+++ Homework1/packer_n_terraform.sh
@@ -1,16 +1,3 @@
 #! /bin/bash 
-packer build template.packer
-if [[ "$?" -eq 0 ]] ; then
-  echo "AWS AMI build by packer done"
-  terraform validate
-  if [[ "$?" -eq 0 ]] ; then
-    echo "Terraform validate done"
-    terraform plan
-    if [[ "$?" -eq 0 ]] ; then
-      echo "Terraform plan done"
-      terraform apply -auto-approve 
-    fi
-    echo "AWS environment build done"
-  fi
-fi
+packer build template.packer && terraform validate && terraform apply -auto-approve 
 
