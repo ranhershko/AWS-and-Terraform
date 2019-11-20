2ae8d0f4c103851ec30f76beea93460ce5d82ce0
Ran Hershko <ran.hershko@gmail.com>
2019-11-21 01:34:28 +0200

    Change to pip3 install instead of apt-get install

--- Homework2/Packer/ansible/ansible.sh
+++ Homework2/Packer/ansible/ansible.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -x
 
-sudo apt-get install python3
-sudo ln -s /usr/bin/python3 /usr/bin/python
+sudo add-apt-repository universe
 sudo apt-get update
-sudo apt-get install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo DEBIAN_FRONTEND=noninteractive apt-get install python3-jinja2 python3-paramiko python3-httplib2 python3-six python3-crypto python3-setuptools python3-cryptography -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install ansible -y
+sudo apt-get upgrade python3
+alias python='/usr/bin/python3'
+apt-cache search python3-pip$
+sudo DEBIAN_FRONTEND=noninteractive apt-get install python3-pip -y
+sudo pip3 install ansible
+#sudo ln -s /usr/bin/python3 /usr/bin/python
+#sudo apt-get update
+#sudo apt-get install software-properties-common
+#sudo apt-add-repository --yes --update ppa:ansible/ansible
+#sudo DEBIAN_FRONTEND=noninteractive apt-get install python3-jinja2 python3-paramiko python3-httplib2 python3-six python3-crypto python3-setuptools python3-cryptography -y
+#sudo DEBIAN_FRONTEND=noninteractive apt-get install python-jinja2 python-yaml python-paramiko python-httplib2 python-six python-crypto python-setuptools sshpass python-cryptography python
+#sudo DEBIAN_FRONTEND=noninteractive apt-get install ansible -y
