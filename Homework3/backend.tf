485897e3a2ddcb09dd6eece9ab4a421e7b3699b2
Ran Hershko <ran.hershko@gmail.com>
2019-11-26 06:47:35 +0200

    Terraform fmt

--- Homework3/backend.tf
+++ Homework3/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     encrypt = true
-    bucket = "opsschool-terraform-remote-state"
-    key = "opschl-ha-web-db-terrform.tfstate"
-    region = "us-east-1"
-    
+    bucket  = "opsschool-terraform-remote-state"
+    key     = "opschl-ha-web-db-terrform.tfstate"
+    region  = "us-east-1"
+
     dynamodb_table = "opsschool-terraform-remote-lock"
   }
 }
--- Homework3/instances.tf
+++ Homework3/instances.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "opschl_ha_web_db" {
   ami                    = count.index < 2 ? data.aws_ami.opschl_ha_web_ami.id : data.aws_ami.opschl_ha_db_ami.id
   subnet_id              = aws_subnet.opschl_ha_web_db-subnet[count.index].id
   vpc_security_group_ids = count.index < 2 ? ["${aws_security_group.opschl_ha_web_db-webnet-sg-allow.id}"] : ["${aws_security_group.opschl_ha_web_db-dbnet-sg-allow.id}"]
-  tags = merge(local.common_tags, { Name = "${var.opschl_tags["prefix_name"]}-${count.index < 2 ? var.list_sub_type[0] : var.list_sub_type[1]}instance_${(count.index % (length(var.list_sub_type))) + 1}" })
+  tags                   = merge(local.common_tags, { Name = "${var.opschl_tags["prefix_name"]}-${count.index < 2 ? var.list_sub_type[0] : var.list_sub_type[1]}instance_${(count.index % (length(var.list_sub_type))) + 1}" })
 }
 
