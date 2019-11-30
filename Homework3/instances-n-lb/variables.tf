bc2e244807f31381bc5f199ca598ec640ffd7af6
Ran Hershko <ran.hershko@gmail.com>
2019-11-30 04:14:32 +0200

    fix ami_id default variable

--- Homework3/instances-n-lb/variables.tf
+++ Homework3/instances-n-lb/variables.tf
@@ -9,5 +9,5 @@ variable "region" {
 }
 
 variable "ami_id" {
-  desfult = (var.public_instance == true ? data.aws_ami.opschl_web_db_ha-web_ami.id : data.aws_ami.opschl_web_db_ha-db_ami.id)
+  default = (var.public_instance == true ? data.aws_ami.opschl_web_db_ha-web_ami.id : data.aws_ami.opschl_web_db_ha-db_ami.id)
 }
