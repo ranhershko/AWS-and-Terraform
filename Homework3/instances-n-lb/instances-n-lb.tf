ba7d720b4a739a814210a9acb7c38326f5be2d23
Ran Hershko <ran.hershko@gmail.com>
2019-11-30 05:20:19 +0200

    Fix instance plan errors

--- Homework3/instances-n-lb/instances-n-lb.tf
+++ Homework3/instances-n-lb/instances-n-lb.tf
@@ -1,7 +1,7 @@
 module "web_instance" {
   source = "../opschl-modules/opschl-base-instance-n-loadbalancer"
   instances_count        = length(data.terraform_remote_state.vpc.outputs.pub_subnet_ids)
-  opschl_tags            = { prefix_name = "opschl-web-db-ha" }
+  opschl_tags            = data.terraform_remote_state.vpc.outputs.project
   instance_key_pair      = "opschl_web_key"
   public_instance        = true
   subnet_ids             = data.terraform_remote_state.vpc.outputs.pub_subnet_ids
@@ -15,18 +15,15 @@ module "web_instance" {
 module "db_instance" {
   source = "../opschl-modules/opschl-base-instance-n-loadbalancer"
   instances_count = length(data.terraform_remote_state.vpc.outputs.priv_subnet_ids)
-  opschl_tags = {prefix_name = "opschl-web-db-ha"}
+  opschl_tags = data.terraform_remote_state.vpc.outputs.project
   instance_key_pair = "opschl_db_key"
   public_instance = false
   subnet_ids = data.terraform_remote_state.vpc.outputs.priv_subnet_ids
   ami_id = data.aws_ami.opschl_web_db_ha-db_ami
   vpc_security_group_ids = data.terraform_remote_state.security.output.priv_sg_ids
-  tags = merge(local.common_tags, {
-    Name = "${var.opschl_tags["prefix_name"]}-${count.index < 2 ? var.list_sub_type[0] : var.list_sub_type[1]}instance_${(count.index % (length(var.list_sub_type))) + 1}"
-  })
   pub_lb_sg_id = ""
   sg_ids = data.terraform_remote_state.security.outputs.priv_sg_ids
-  vpc_id = ""
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
 resource "aws_lb" "opschl_ha_web_db-webPublic" {
