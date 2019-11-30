12a7281962e4f1f5c636f269070d0218a558604c
Ran Hershko <ran.hershko@gmail.com>
2019-11-30 04:11:06 +0200

    add apt main repo

--- Homework3/Packer/ansible/ansible.sh
+++ Homework3/Packer/ansible/ansible.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 
-sudo add-apt-repository universe
-sudo add-apt-repository multiverse
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+#sudo add-apt-repository universe
+#sudo add-apt-repository multiverse
 sudo apt-get update -y
 sudo apt-cache madison python3 aptitude python3-pip
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade python3 aptitude python3-pip -y
-sudo pip3 install ansible
+sudo -H pip3 install ansible
 
