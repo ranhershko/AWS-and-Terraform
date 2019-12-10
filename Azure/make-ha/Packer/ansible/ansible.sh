3e7281ef07b50126d6a7e95441d9444c623d5dfe
Ran Hershko <ran.hershko@gmail.com>
2019-12-10 08:56:12 +0200

    Rename ansible installation file

--- Azure/make-ha/Packer/ansible/ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-
-sudo yum update -y
-sudo apt-cache madison python3 aptitude python3-pip
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade python3 aptitude python3-pip -y
-sudo -H pip3 install ansible
-
