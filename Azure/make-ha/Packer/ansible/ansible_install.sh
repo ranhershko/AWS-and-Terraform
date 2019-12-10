e9c2a0596d71f3556c755e6008c611b801466a35
Ran Hershko <ran.hershko@gmail.com>
2019-12-11 01:08:44 +0200

    Change centos ansible install

--- Azure/make-ha/Packer/ansible/ansible_install.sh
+++ Azure/make-ha/Packer/ansible/ansible_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 sudo yum update -y
-sudo yum install python3 python3-pip -y
-sudo -H pip3 install ansible
+sudo yum install -y ansible
+#sudo yum install python3 python3-pip -y
+#sudo -H pip3 install ansible
 
