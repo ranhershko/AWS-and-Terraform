a55b9803ce6222a58450217d19750706b52b8f69
Ran Hershko <ran.hershko@gmail.com>
2020-01-07 16:10:48 +0200

    Create azure instances variable file

---
+++ Azure/make-ha/Instances/instances_variables.tf
@@ -0,0 +1,20 @@
+variable "location" {}
+variable "project" {}
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+
+variable "resources_count" {}
+variable "vnet_address_space" {}
+
+variable "stor_account_repl_type" {}
+variable "stor_account_tier" {}
+
+variable "availability_set_domain_count" {}
+variable "admin_username" {}
+variable "managed_disk_type" {}
+variable "os_type" {}
+variable "packer_rg" {}
+variable "vm_size" {}
